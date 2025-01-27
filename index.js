342c5478a865aef68abda6dab650ec27beb13b5d
Enoch UWUMUKIZA <wwwenockuwumukiza@gmail.com>
2025-01-27 12:18:29 +0000

    adding uploading functionalities

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
 
 var app = express();
+
+var storage = multer.memoryStorage();
+var upload = multer({storage}).single('upfile');
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +15,28 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload, (req,res) =>{
+  
+  const file = req?.file;
+
+  if(file){
+
+    const fileMetadata = {
+      name:req?.file?.originalname,
+      type:req?.file?.mimetype,
+      size:req?.file?.size
+    }
+
+    res.send(fileMetadata);
+
+    console.log('file submitted');
+
+  }else{
+    console.log('no file')
+  }
+
+
+})
 
 
 
